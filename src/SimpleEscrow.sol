2e7c9792d8dc914951501bd6eb4aca21a0912c70
Olumuyiwa Aderemi <olumuyiwaworks02@gmail.com>
2025-10-08 17:34:12 +0100

    Update SimpleEscrow.sol

--- src/SimpleEscrow.sol
+++ src/SimpleEscrow.sol
@@ -18,18 +18,18 @@ contract MicroLegalAidFund {
         legalAidWallet = _legalAidWallet;
     }
 
-    // Function to donate
+ 
     function donate(string calldata _name) external payable {
         require(msg.value > 0, "Donation must be greater than 0");
 
-    // Save/update donor record
+    
         donors[msg.sender].amount += msg.value;
         donors[msg.sender].name = _name;
 
         emit DonationReceived(msg.sender, _name, msg.value);
     }
 
-    // Function to withdraw (only legal aid wallet)
+    
     function withdraw(uint _amount) external {
         require(msg.sender == legalAidWallet, "Not authorized");
         require(address(this).balance >= _amount, "Insufficient funds");
@@ -37,12 +37,12 @@ contract MicroLegalAidFund {
         emit FundsWithdrawn(msg.sender, _amount);
     }
 
-    // Check contract balance
+   
     function getBalance() external view returns (uint) {
         return address(this).balance;
     }
 
-    // View donor info
+   
     function getDonor(address _donor) external view returns (string memory, uint) {
         Donor memory d = donors[_donor];
         return (d.name, d.amount);
